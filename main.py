7d274d5fe0ee4a81895f11f5c793907806469cd9
Michał Walkowski <mi.walkowski@gmail.com>
2024-01-20 17:57:51 +0100

    project update

--- main.py
+++ main.py
@@ -1,71 +1,116 @@
+import re
 import base64
-import json
+import datetime
+import queue
+from functools import wraps
 
 from flask import Flask
+from flask import make_response as flask_make_response
 from flask import jsonify
 from flask import request
+from requests import status_codes
+
+from datetime import datetime, timezone
 
 from Crypto.Signature import pkcs1_15 as pkcs_signature
 from Crypto.Hash import SHA256
-from Crypto.Cipher import PKCS1_v1_5 as pkcs_cipher
 from Crypto.PublicKey import RSA
 
-PRIV_KEY = 'private.key'
-PUB_KEY = 'public.pem'
+PRIVATE_KEY_PATH = 'private.key'
+PUBLIC_KEY_PATH = 'public.pem'
 SIGNATURE_HEADER = 'X-Signature'
 NONCE_HEADER = 'X-Nonce-Value'
+NONCE_QUEUE_SIZE_LIMIT = 10
 NONCE_CREATED_AT_HEADER = 'X-Nonce-Created-At'
+TIME_DIFF_TOLERANCE_IN_SECONDS = 10.0
 
+
+PUBLIC_KEY = RSA.import_key(open(PUBLIC_KEY_PATH).read())
+PRIVATE_KEY = RSA.import_key(open(PRIVATE_KEY_PATH).read())
+UUID_PATTERN = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$"
+
+received_nonces = queue.Queue()
 app = Flask(__name__)
 
 
 def check_headers_exists():
-    if SIGNATURE_HEADER not in request.headers:
-        return SIGNATURE_HEADER
     if NONCE_HEADER not in request.headers:
         return NONCE_HEADER
     if NONCE_CREATED_AT_HEADER not in request.headers:
         return NONCE_CREATED_AT_HEADER
+    if SIGNATURE_HEADER not in request.headers:
+        return SIGNATURE_HEADER
     return None
+
+
+def is_nonce_created_at_correct():
+    try:
+        nonce_created_at = request.headers[NONCE_CREATED_AT_HEADER]
+        time_diff = datetime.now().astimezone(timezone.utc) - datetime.fromisoformat(nonce_created_at)
+        return time_diff.total_seconds() < TIME_DIFF_TOLERANCE_IN_SECONDS
+    except Exception:
+        return False
+
+
+def clean_up_the_received_nonces_queue():
+    if received_nonces.qsize() >= NONCE_QUEUE_SIZE_LIMIT:
+        received_nonces.get(block=True)
+
+
+def is_nonce_correct():
+    nonce = request.headers[NONCE_HEADER]
+    if re.match(UUID_PATTERN, nonce) and nonce not in received_nonces.queue:
+        clean_up_the_received_nonces_queue()
+        received_nonces.put(nonce, block=True)
+        return True
+    return False
 
 
 def verify(message, signature):
     h = SHA256.new(message)
-    pkey = RSA.import_key(open(PRIV_KEY).read())
-
-    print('SIGNATURE EXPECTED', base64.standard_b64encode(pkcs_signature.new(pkey).sign(h)).decode('utf-8'))
-    print('SIGNATURE RECEIVED', signature)
-
-    key = RSA.import_key(open(PUB_KEY).read())
     try:
-        pkcs_signature.new(key).verify(h, base64.standard_b64decode(signature))
+        pkcs_signature.new(PUBLIC_KEY).verify(h, base64.standard_b64decode(signature))
     except (ValueError, TypeError) as e:
-        print(e)
         return False
     return True
 
 
-def check_signature():
+def is_signature_correct():
     nonce_value = request.headers[NONCE_HEADER]
     nonce_created_at = request.headers[NONCE_CREATED_AT_HEADER]
     signature_input = "{}{}{}{}{}".format(request.method, request.path, nonce_value, nonce_created_at, request.data.decode())
-    print('signature_input', signature_input)
-    signature_input_b64 = base64.standard_b64encode(signature_input.encode()).decode()
-    print('signature_input_b64', signature_input_b64)
-    return verify(signature_input_b64.encode(), request.headers[SIGNATURE_HEADER])
+    signature_input_b64 = base64.standard_b64encode(signature_input.encode())
+    return verify(signature_input_b64, request.headers[SIGNATURE_HEADER])
+
+
+def make_response(msg):
+    return flask_make_response(jsonify({"error": msg}), status_codes.codes.forbidden)
+
+
+def signature_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        missing_header = check_headers_exists()
+
+        if missing_header:
+            return make_response(f"Missing header: {missing_header}")
+
+        if not is_nonce_correct():
+            return make_response("Nonce is already used or incorrect")
+
+        if not is_nonce_created_at_correct():
+            return make_response("The request is time-barred")
+
+        if not is_signature_correct():
+            return make_response("Invalid Signature")
+
+        return f(*args, **kwargs)
+    return decorator
 
 
 @app.route("/signed-body", methods=["POST"])
+@signature_required
 def signed_body():
-    missing_header = check_headers_exists()
-
-    if missing_header:
-        return jsonify({"error": f"Missing header: {missing_header}"})
-
-    signature_ok = check_signature()
-    if not signature_ok:
-        return jsonify({"error": f"Invalid Signature"})
-
     return jsonify({"msg": "Ok!"})
 
 
---
+++ tests.py
@@ -0,0 +1,154 @@
+import base64
+import datetime
+import json
+import uuid
+
+import pytest
+
+from Crypto.Hash import SHA256
+from Crypto.Signature import pkcs1_15 as pkcs_signature
+
+from main import app as test_app, received_nonces, PRIVATE_KEY
+from main import NONCE_HEADER, NONCE_CREATED_AT_HEADER, SIGNATURE_HEADER, TIME_DIFF_TOLERANCE_IN_SECONDS
+
+@pytest.fixture()
+def app():
+    test_app.config.update({
+        "TESTING": True,
+    })
+    yield test_app
+
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+
+def test_NONCE_HEADER_not_exists(client):
+    response = client.post("/signed-body", json={})
+
+    assert response.status_code == 403
+    assert response.json == {'error': F'Missing header: {NONCE_HEADER}'}
+
+
+def test_NONCE_CREATED_AT_HEADER_not_exists(client):
+    response = client.post("/signed-body", headers={NONCE_HEADER: "a"}, json={})
+
+    assert response.status_code == 403
+    assert response.json == {'error': F'Missing header: {NONCE_CREATED_AT_HEADER}'}
+
+
+def test_SIGNATURE_HEADER_not_exists(client):
+    headers = {
+        NONCE_HEADER: "a",
+        NONCE_CREATED_AT_HEADER: "b",
+    }
+    response = client.post("/signed-body", headers=headers, json={})
+
+    assert response.status_code == 403
+    assert response.json == {'error': F'Missing header: {SIGNATURE_HEADER}'}
+
+
+def test_NONCE_HEADER_is_incorrect(client):
+    headers = {
+        NONCE_HEADER: "a",
+        NONCE_CREATED_AT_HEADER: "b",
+        SIGNATURE_HEADER: "aaaa"
+    }
+    response = client.post("/signed-body", headers=headers, json={})
+
+    assert response.status_code == 403
+    assert response.json == {'error': "Nonce is already used or incorrect"}
+
+
+def test_NONCE_HEADER_pool(client):
+    for _ in range(30):
+        headers = {
+            NONCE_HEADER: uuid.uuid4(),
+            NONCE_CREATED_AT_HEADER: "b",
+            SIGNATURE_HEADER: "aaaa"
+        }
+        response = client.post("/signed-body", headers=headers, json={})
+
+        assert response.status_code == 403
+    assert received_nonces.qsize() == 10
+
+
+def test_NONCE_HEADER_is_duplicated(client):
+    headers = {
+        NONCE_HEADER: uuid.uuid4(),
+        NONCE_CREATED_AT_HEADER: "b",
+        SIGNATURE_HEADER: "aaaa"
+    }
+    response = client.post("/signed-body", headers=headers, json={})
+
+    assert response.status_code == 403
+    assert response.json == {'error': "The request is time-barred"}
+
+    response = client.post("/signed-body", headers=headers, json={})
+
+    assert response.status_code == 403
+    assert response.json == {'error': "Nonce is already used or incorrect"}
+
+
+def test_NONCE_CREATED_AT_HEADER_invalid(client):
+    headers = {
+        NONCE_HEADER: uuid.uuid4(),
+        NONCE_CREATED_AT_HEADER: "b",
+        SIGNATURE_HEADER: "aaaa"
+    }
+    response = client.post("/signed-body", headers=headers, json={})
+
+    assert response.status_code == 403
+    assert response.json == {'error': "The request is time-barred"}
+
+def test_NONCE_CREATED_AT_HEADER_time_barred(client):
+    nonce_created_at = datetime.datetime.now(datetime.timezone.utc)
+    nonce_created_at -= datetime.timedelta(days=0, seconds=TIME_DIFF_TOLERANCE_IN_SECONDS + 1)
+    headers = {
+        NONCE_HEADER: uuid.uuid4(),
+        NONCE_CREATED_AT_HEADER: nonce_created_at.isoformat(),
+        SIGNATURE_HEADER: "aaaa"
+    }
+    response = client.post("/signed-body", headers=headers, json={})
+
+    assert response.status_code == 403
+    assert response.json == {'error': "The request is time-barred"}
+
+def test_SIGNATURE_invalid(client):
+    headers = {
+        NONCE_HEADER: uuid.uuid4(),
+        NONCE_CREATED_AT_HEADER: datetime.datetime.now(datetime.timezone.utc).isoformat(),
+        SIGNATURE_HEADER: "aaaa"
+    }
+    response = client.post("/signed-body", headers=headers, json={'msg': 'msg'})
+
+    assert response.status_code == 403
+    assert response.json == {'error': "Invalid Signature"}
+
+
+def test_SIGNATURE_ok(client):
+    nonce = uuid.uuid4()
+    nonce_created_at = datetime.datetime.now(datetime.timezone.utc).isoformat()
+    request_data = json.dumps({'msg': 'msg'})
+
+    signature_input = "{}{}{}{}{}".format('POST', '/signed-body', nonce, nonce_created_at, request_data)
+    signature_input_b64 = base64.standard_b64encode(signature_input.encode())
+    h = SHA256.new(signature_input_b64)
+    signature = base64.standard_b64encode(pkcs_signature.new(PRIVATE_KEY).sign(h)).decode('utf-8')
+
+    headers = {
+        NONCE_HEADER: nonce,
+        NONCE_CREATED_AT_HEADER: nonce_created_at,
+        SIGNATURE_HEADER: signature
+    }
+    response = client.post("/signed-body", headers=headers, json=json.loads(request_data))
+
+    assert response.status_code == 200
+    assert response.json == {"msg": "Ok!"}
